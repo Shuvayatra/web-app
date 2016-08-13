55c882a8bcd002094d5e7813d20e837711141e24
Yohan Totting <tyohan@gmail.com>
2016-08-13 21:10:02 +0700

    fixing import behavior on shuvayatra-header.html add extra config to include src, pages, etc on sw

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,12 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/src/*.html',
+    '/src/behaviors/*.html',
+    '/pages/*.html',
+    '/images/**.*'
+
   ],
   navigateFallback: '/index.html'
 };
