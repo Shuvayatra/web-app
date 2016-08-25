4dd158fa3fce37951f46144e8132bb2ff5e6d796
Yohan Totting <tyohan@gmail.com>
2016-08-25 22:07:27 +0545

    setting service workers to push offline first

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -9,5 +9,18 @@ module.exports = {
     '/images/**.*'
 
   ],
-  navigateFallback: '/index.html'
+  navigateFallback: '/index.html',
+  runtimeCaching: [{
+    urlPattern: /^https:\/\/api\.shuvayatra\.org\/api/,
+    handler: 'fastest'
+  }, {
+    urlPattern: /^https:\/\/fonts\.googleapis\.com\/css/,
+    handler: 'fastest'
+  }, {
+    urlPattern: /^https:\/\/api\.shuvayatra\.org\/uploads/,
+    handler: 'fastest'
+  }, {
+    urlPattern: /\.(jpg|jpeg|gif|png|css|js|ico|xml|rss|txt|mp3|svg)$/i,
+    handler: 'fastest',
+  }]
 };
