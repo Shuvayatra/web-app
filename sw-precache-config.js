bfeb52eb7a45606534fa771f0ab30c8a6cc17048
Yohan Totting <tyohan@gmail.com>
2016-09-12 22:49:10 +0700

    modified build config

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   staticFileGlobs: [
-    '/index.html',
-    '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
-    '/src/*.html',
-    '/src/behaviors/*.html',
-    '/pages/*.html',
-    '/images/**.*'
+    'index.html',
+    'manifest.json',
+    'bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    'src/*.html',
+    'src/behaviors/*.html',
+    'pages/*.html',
+    'images/**.*'
 
   ],
   navigateFallback: '/index.html',
